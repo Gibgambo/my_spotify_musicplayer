c0893ad8d0a14fa4f784bd3d41cb62eb584a5de9
Jeff <gibgambo@gmail.com>
2025-08-17 00:22:02 +0200

    initial commit

---
+++ src/main/java/my/musicplayer/spotify_player/SecurityConfig.java
@@ -0,0 +1,22 @@
+package my.musicplayer.spotify_player;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+      .csrf(csrf -> csrf.disable())
+      .authorizeHttpRequests(auth -> auth
+        .requestMatchers("/", "/index.html", "/css/**", "/js/**", "/images/**", "/favicon.ico").permitAll()
+        .anyRequest().permitAll()
+      );
+    return http.build();
+  }
+}
+
---
+++ src/test/java/my/musicplayer/spotify_player/SpotifyPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package my.musicplayer.spotify_player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
